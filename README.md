17669ae9676f927ce493e16a577eceb0dfc5ea0e
Rk-Thakur <tranjan638@gmail.com>
2022-04-26 21:52:46 +0545

    UI Project

---
+++ README.md
@@ -0,0 +1,11 @@
+# Restaurant UI
+
+Restaurant UI : Tried my best
+
+## Getting Started
+
+## ScreenShots:
+
+![alt text](assets/images/first.jpg)
+![alt text](assets/images/second.jpg)
+![alt text](assets/images/third.jpg)
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:get/route_manager.dart';
+import 'package:projectui/screens/detailscreen.dart';
+import 'package:projectui/screens/main_screen.dart';
+import 'package:projectui/screens/userscreen.dart';
+
+void main() {
+  runApp(ProviderScope(child: myapp()));
+}
+
+class myapp extends StatelessWidget {
+  const myapp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      home: mainScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
